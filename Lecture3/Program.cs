6fc3e34d9aea6be425cc492b26bd8394d6ccdacf
davidbabayan <david.babayan@hotmail.com>
2020-02-17 12:46:51 +0400

    Lecture 3 Final

---
+++ Lecture3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace Lecture3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("What is your name? ");
+            //Console.ReadLine();
+            WriteLine("Hello " + ReadLine() + "!");
+        }
+    }
+}
