1d63e7f11d85f335413ad23d263dafeccbcdfa22
davidbabayan <david.babayan@hotmail.com>
2020-02-21 13:02:51 +0400

    Lecture 4

---
+++ Lecture4/NameAndYear/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NameAndYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            Console.WriteLine($"My name is {Console.ReadLine()}!");
+            Console.Write("How old are you?");
+            Console.WriteLine($"You are {Console.ReadLine()} years old!");
+        }
+    }
+}
+
+//1. Ask a question "What is your name?" +
+//2. Print "My Name is {User Response}!" +
+//3. Ask a question "What is your age?"
+//4. Print "You are {User Response} years old!"
---
+++ Lecture4/Variables/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ամբողջ թվեր int
+            int i = 34;
+            //կոտորակային թվեր double
+            double d = 12.5;
+            //այո/ոչ արժեք bool
+            bool b = true; //true/false
+            //սիմվոլներ char
+            char ch = 'c';
+            //տեքստ string
+            string s = "Some Text";
+
+            //Printing Name and Age
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            
+            Console.Write("How old are you? ");
+            int age = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Your name is {name} and you are {age} years old!");
+
+            //Arithmetic operations
+
+            // գումարում +
+            // հանում -
+            // բաժանում /
+            // բազմապատկում *
+            // մնացորդ %
+
+            Console.WriteLine("Hello, " + "World!");
+        }
+    }
+}
